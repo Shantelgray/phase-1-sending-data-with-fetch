4c193ba80ffb2bcfacf6f4eb292187e976211b51
Shantel <Shantelgray24@gmail.com>
2025-05-20 13:38:05 -0400

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((response) => appendToDOM(response.id))
+    .catch((response) => appendToDOM(response.message));
+}
+
+const appendToDOM = (data) => {
+  const newElement = document.createElement("h1");
+  newElement.textContent = data;
+  document.body.appendChild(newElement);
+  return data;
+};
